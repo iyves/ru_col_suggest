7e6f1cf332beca7e8e6be3401a948335fc351ebd
Lenovo Windows - Yves Wienecke <whyves@icloud.com>
2020-09-15 11:21:42 -0700

    Add scripts for extracting text from pdfs and preprocessing.
    Signed-off-by: Lenovo Windows - Yves Wienecke <whyves@icloud.com>

---
+++ ru_col_suggest/scripts/preprocessing/kutuzov/make_txt_from_pdf.py
@@ -0,0 +1,57 @@
+from os import path, walk, makedirs
+from io import StringIO
+from pdfminer3.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+from pdfminer3.layout import LAParams
+from pdfminer3.pdfpage import PDFPage
+
+
+from pdfminer3.pdfdocument import PDFDocument
+def convert_pdf_to_txt(filepath):
+    rm = PDFResourceManager()
+    sio = StringIO()
+    device = TextConverter(rm, sio, codec='utf-8', laparams=LAParams())
+    interpreter = PDFPageInterpreter(rm, device)
+    with open(filepath, 'rb') as fp:
+        for page in PDFPage.get_pages(fp=fp, pagenos=set(), maxpages=0, password='',
+                                      caching=True, check_extractable=True):
+            interpreter.process_page(page)
+    text = sio.getvalue()
+    device.close()
+    sio.close()
+    return text
+
+
+def write_errors(errors):
+    with open('errors.txt', 'w', encoding='utf-8') as f:
+        for error in errors:
+            f.write('{}\n'.format(error))
+
+
+def write_file(root, file, text):
+    with open(path.join(root.replace(source_dir, saving_dir), file).replace('.pdf', '.txt'), 'w',
+              encoding='utf-8') as f:
+        f.write(text)
+
+
+def convert(source_dir, saving_dir):
+    errors = []
+    for root, dirs, files in walk(source_dir):
+        for file in files:
+            if not file.endswith('pdf'):
+                errors.append(path.join(root, file))
+                continue
+            text = convert_pdf_to_txt(path.join(root, file))
+            try:
+                write_file(root, file, text)
+            except FileNotFoundError:
+                makedirs(path.dirname(path.join(root.replace(source_dir, saving_dir), file)))
+                write_file(root, file, text)
+            print("converted", file, "to", saving_dir)
+    write_errors(errors)
+
+
+if __name__ == '__main__':
+    saving_dir = 'parsed'
+    source_dir = 'sources'
+    convert(source_dir, saving_dir)
