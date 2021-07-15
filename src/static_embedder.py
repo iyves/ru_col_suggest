b5fe528f7eb9d9d286ef80a40385031844ca3b03
Lenovo Windows - Yves Wienecke <whyves@icloud.com>
2021-07-15 16:13:05 -0700

    Implemented collocation suggesting via static word embeddings.

--- src/static_embedder.py
+++ src/static_embedder.py
@@ -6,7 +6,8 @@ import os
 import sys
 
 from enum import Enum
-from gensim.models import FastText, Word2Vec
+from gensim.models import FastText, KeyedVectors, Word2Vec
+from itertools import product
 from pathlib import Path
 from typing import Iterable, List, Optional, Tuple
 
@@ -37,11 +38,70 @@ class StaticEmbedder():
     def __init__(self, model_type, src: str):
         self.model_type = model_type
         self.model = self.load_model(src)
-        pass
 
     def load_model(self, src: str):
-        pass
+        if self.model_type == self.Model.WORD2VEC:
+            return Word2Vec.load(src)
+        elif self.model_type == self.Model.FASTTEXT:
+            return FastText.load(src)
+        elif self.model_type == self.Model.GLOVE:
+            return KeyedVectors.load_word2vec_format(src, binary=True, unicode_errors='ignore')
+        else:
+            logging.error("Can only load w2v, fasttext, or glove models.")
+            raise NotImplementedError
 
-    def suggest_collocations(self, ngram_tokens: List[str], fixed_positions: List[int]) \
-            -> List[Tuple[str, int, float]]:
-        pass
+    def suggest_collocations(self, ngram_tokens: List[str], fixed_positions: List[int],
+                             topn=10, cossim = True) -> List[Tuple[str, int, float]]:
+        # Filter out the tokens for which replacements will be suggested
+        if len(ngram_tokens) != len(fixed_positions):
+            logging.error(f"Length of ngram_tokens ({len(ngram_tokens)}) is not equal to length of fixed_positions ({len(fixed_positions)})")
+            return []
+        tokens_to_replace = [ngram_tokens[i] for (i, fixed) in enumerate(fixed_positions) if fixed == 0]
+
+        # W2v and GloVe models
+        if self.model_type == self.Model.WORD2VEC or self.model_type == self.Model.GLOVE:
+           # Ensure that the token is in the w2v vocabulary
+           for token in tokens_to_replace:
+               if token not in self.model.wv.vocab:
+                   logging.error(f"'{token}' not in model vocabulary.")
+                   return []
+        elif self.model_type == self.Model.FASTTEXT:
+            # No extra work to do for fastText model
+            for token in tokens_to_replace:
+                if token not in self.model.wv.key_to_index:
+                    logging.info(f"'{token}' not in model vocabulary.")
+        else:
+            logging.error("Can suggest collocations for only w2v, fasttext, or glove models.")
+            raise NotImplementedError
+        # Get the similar words in format (token, pos tag, cosine distance)
+        if cossim:
+            similar_words = enumerate([tuple(token[0].split("_")) + (token[-1],)
+                                       for token in self.model.wv.most_similar(positive=tokens_to_replace, topn=topn)])
+        else:
+            similar_words = enumerate([tuple(token[0].split("_")) + (token[-1],)
+                                       for token in self.model.wv.most_similar_cosmul(positive=tokens_to_replace, topn=topn)])
+
+        # Filter similar words by PoS tags in format (token, rank, cosine distance)
+        suggested_replacements = []
+        for ngram, fixed in zip(ngram_tokens, fixed_positions):
+            if fixed == 1:
+                suggested_replacements.append([tuple(ngram.split("_")[0], 0, 0)])
+            else:
+                # Get the similar words in format (token, pos tag, cosine distance)
+                pos_tag = ngram.split("_")[1]
+                suggested_replacements.append([
+                    (token[1][0], token[0], token[1][2]) for token in similar_words
+                        if token[1][1] == pos_tag
+                ])
+
+        suggested_collocations = []
+        for colloc_tuple in product(*suggested_replacements):
+            colloc = []
+            rank = 0
+            cosine = 0
+            for tuple in colloc_tuple:
+                colloc.append(tuple[0])
+                rank += tuple[1]
+                cosine += tuple[2]
+            suggested_collocations.append((" ".join(colloc), rank, cosine))
+        return suggested_collocations
