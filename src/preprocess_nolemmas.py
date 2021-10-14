e55285fa31854fac1e48380699c32e9491b59394
lauberto <andrea.grillandi@live.it>
2021-10-14 10:53:45 +0300

    add script to preprocess text without producing lemmas for it

---
+++ src/preprocess_nolemmas.py
@@ -0,0 +1,73 @@
+import configparser
+import logging
+import os
+
+from joblib import delayed, Parallel
+from pathlib import Path
+from helpers import get_file_names, get_text
+from html_preprocessor import HtmlPreprocessor
+from tokenizer import Tokenizer
+
+# Set up the configuration
+path_current_directory = os.path.dirname(__file__)
+path_config_file = os.path.join(path_current_directory, '../',
+                                'config.ini')
+config = configparser.ConfigParser()
+config.read(path_config_file)
+data_dir = config['PATHS']['data_dir']
+log_dir = config['PATHS']['log_dir']
+log_file = str(Path(log_dir, 'preprocess.txt'))
+logging.basicConfig(handlers=[logging.FileHandler(log_file, 'a', 'utf-8')],
+                    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+def preprocess_one(filename, file, target_txt_path) -> bool:
+    """Preprocess one scholarly paper and save the text and tokens.
+
+    :param filename: The name of the file to preprocessed.
+    :param file: The path to the file to processed.
+    :param target_txt_path: The path to save the preprocessed text.
+    :param target_token_path: The path to save the preprocessed tokens.
+    :param method: The method for lemmatizing the text. Default: TREETAGGER
+    :return: True if successfully preprocessed the file, otherwise False
+    """
+    text = get_text(file)
+    preprocessor = HtmlPreprocessor(text, filename)
+    if not preprocessor.preprocess():
+        logging.error("Error: Failed to preprocesses file: {}".format(filename))
+        return False
+
+    logging.info("Preprocessed html -> txt: {}".format(filename))
+    # Save preprocessed text as txt file
+    output_txt_file = Path(target_txt_path, filename + ".txt")
+    if not os.path.isfile(output_txt_file):
+        with open(output_txt_file, "w", encoding='utf8') as file:
+            file.write(preprocessor.get_text())
+            
+    logging.info("Preprocessed and tokenized file: {}".format(filename))
+    print(filename, end=", ")
+    return True
+
+
+for folder in 'Economics_2 Law_2'.split():
+    logging.info("Preprocessing papers from folder: {}".format(folder))
+    print("\n\n", folder)
+
+    # Set up the target directories, to where the preprocessed text will be saved
+    source = str(Path(data_dir, "extracted/html/{}".format(folder)).resolve())
+    target_txt = str(Path(data_dir, "preprocessed/text/{}".format(folder)).resolve())
+    target_tokens = str(Path(data_dir, "preprocessed/tokens/{}".format(folder)).resolve())
+    for target in [target_txt, ]:
+        if not os.path.exists(target):
+            os.makedirs(target)
+
+    # Set up the files to preprocess
+    files = [(os.path.splitext(os.path.basename(file))[0], file)
+             for file in get_file_names(source, ".html")]
+
+    # Preprocess each file in parallel
+    # for filename, file in files:
+    #     preprocess_one(filename, file, target_txt)
+    element_information = Parallel(n_jobs=-1)(
+        delayed(preprocess_one)(filename, file, target_txt) for filename, file in files)
+    print("Done!")
